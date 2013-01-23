de6952e5a6bd839ff73c736ea4f6144749f2f09b
Trevor Alpeter <trevtor@gmail.com>
2013-01-22 20:43:55 -0500

    Removes old test definition.

--- test/spec/test.app.js
+++ test/spec/test.app.js
@@ -1,21 +1,3 @@
-define(['app/app'], function(app) {
-	describe('App', function() {
-		it('Should define a \'header\' region', function() {
-			expect(app.headerRegion).to.exist;
-		});
-
-		it('Should define a \'main\' region', function() {
-			expect(app.mainRegion).to.exist;
-		});
-	});
-
-	return {
-		name: "App"
-	};
-});
-
-
-/*
 define(['Squire'], function(Squire) {
 	describe('App', function() {
 
@@ -42,4 +24,3 @@ define(['Squire'], function(Squire) {
 		name: "App"
 	};
 });
-*/
